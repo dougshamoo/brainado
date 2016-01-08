8dc2dccd045b2a5922bf870bc14bb2a0b567f213
Doug Shamoo <doug.shamoo@gmail.com>
2016-01-07 21:53:58 -0800

    (feat) Add POST route for word and API call [Closes #4][Closes #9]

--- app/entry.js
+++ app/entry.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var request = require('superagent');
 
 
 var WordInput = React.createClass({
@@ -9,25 +10,83 @@ var WordInput = React.createClass({
     }
   },
   handleChange: function(e) {
-    this.setState(e.target.value);
+    this.setState({value: e.target.value});
   },
-  handleSubmit: function(e) {
-    // api call
+  handleSubmit: function(word, e) {
+    e.preventDefault();
+    this.props.submitWord(word);
   },
   render: function() {
     return (
-      <input type="text" 
-        value={this.state.value} 
-        onChange={this.handleChange}
-        onSubmit={this.handleSubmit}/>
+      <form onSubmit={this.handleSubmit.bind(this, this.state.value)}>
+        <input type="text" 
+          value={this.state.value}
+          onChange={this.handleChange} />
+        <input type="submit" value="Submit" />
+      </form>
     );
   }
 });
 
-var App = React.createClass({
+var WordList = React.createClass({
+  getInitialState: function() {
+    return {
+      words: this.props.words,
+    }
+  },
+  componentWillReceiveProps: function(nextProps) {
+    this.setState({
+      words: nextProps.words,
+    });
+  },
+  renderWords: function() {
+    return this.state.words.map(function(word) {
+      return (
+        <li key={word}>{word}</li>
+      );
+    });
+  },
   render: function() {
     return (
-      <WordInput/>
+      <div>
+        <ul>
+          {this.renderWords()}
+        </ul>
+      </div>
+    );
+  },
+});
+
+var App = React.createClass({
+  getInitialState: function() {
+    return {
+      words: [],
+    }
+  },
+  submitWord: function (word) {
+    //post word to server
+    console.log('post ' + word + ' to server');
+    var self = this;
+    request
+      .post('/word')
+      .send({word: word})
+      .end(function(err, data) {
+        if (err || !data.ok){
+          console.log('post request for ' + word + ' failed');
+        } else {
+          console.log('post request successful: ' + data.body.associations_array);
+          self.setState({words: data.body.associations_array});
+        }
+      });
+  },
+  render: function() {
+    return (
+      <div>
+        <WordInput
+          submitWord={this.submitWord}/>
+        <WordList 
+          words={this.state.words}/>
+      </div>
     );
   },
 });
