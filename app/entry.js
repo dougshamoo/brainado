ac5e9456c47b3ad1b0c6b03cefcd8dcc7c6331dc
Doug Shamoo <doug.shamoo@gmail.com>
2016-01-15 19:03:16 -0800

    (refactor) Invert distance to word association scores [Closes #17]
    -plus minor style cleanup

--- app/entry.js
+++ app/entry.js
@@ -30,24 +30,24 @@ var TEST_DATA = {
     {
       source: 0,
       target: 1,
-      score: 50,
+      score: .5,
     },
     {
       source: 0,
       target: 2,
-      score: 100,
+      score: 1,
     },
     {
       source: 0,
       target: 3,
-      score: 150,
+      score: 1.5,
     },
     {
       source: 0,
       target: 4,
-      score: 200,
+      score: 2,
     },
-  ]
+  ],
 };
 
 var WordInput = React.createClass({
@@ -56,36 +56,41 @@ var WordInput = React.createClass({
       value: 'Hello!',
     };
   },
+
   handleChange: function(e) {
     this.setState({value: e.target.value});
   },
+
   handleSubmit: function(word, e) {
     e.preventDefault();
     this.props.submitWord(word);
   },
+
   render: function() {
     return (
       <form onSubmit={this.handleSubmit.bind(this, this.state.value)}>
-        <input type="text" 
+        <input type="text"
           value={this.state.value}
           onChange={this.handleChange} />
         <input type="submit" value="Submit" />
       </form>
     );
-  }
+  },
 });
 
 var WordList = React.createClass({
   getInitialState: function() {
     return {
       associatedWords: this.props.associatedWords,
-    }
+    };
   },
+
   componentWillReceiveProps: function(nextProps) {
     this.setState({
       associatedWords: nextProps.associatedWords,
     });
   },
+
   renderWords: function() {
     return this.state.associatedWords.map(function(word) {
       return (
@@ -93,6 +98,7 @@ var WordList = React.createClass({
       );
     });
   },
+
   render: function() {
     return (
       <div>
@@ -114,12 +120,12 @@ var Node = React.createClass({
         cx={this.props.x}
         cy={this.props.y}
         style={{
-          "fill": color(this.props.group),
-          "stroke":"#fff",
-          "strokeWidth":"1.5px"
+          fill: color(this.props.group),
+          stroke:'#fff',
+          strokeWidth:'1.5px',
         }}/>
     );
-  }
+  },
 });
 
 var Link = React.createClass({
@@ -131,12 +137,12 @@ var Link = React.createClass({
         x2={this.props.datum.target.x}
         y2={this.props.datum.target.y}
         style={{
-          "stroke":"#999", 
-          "strokeOpacity":".6",
-          "strokeWidth": "2"  
+          stroke:'#999',
+          strokeOpacity:'.6',
+          strokeWidth: '2',
         }}/>
     );
-  }
+  },
 });
 
 var NodeLabel = React.createClass({
@@ -148,74 +154,83 @@ var NodeLabel = React.createClass({
         {this.props.text}
       </text>
     );
-  }
+  },
 });
 
-var Graph = React.createClass({ 
+var Graph = React.createClass({
   // mixins: [Radium.StyleResolverMixin, Radium.BrowserStateMixin],
   getInitialState: function() {
-    
-  var svgWidth = 900;
-  var svgHeight = 900; 
-  var force = d3.layout.force()
-    .charge(-120)
-    .linkDistance(function(d) {
-      return d.score;
-    })
-    .size([svgWidth, svgHeight]);
-    
+    var svgWidth = 900;
+    var svgHeight = 900;
+    var force = d3.layout.force()
+      .charge(-120)
+      .linkDistance(function(d) {
+        // Distance is inversely proportional to the association score,
+        // multiplied by a scaling value to spread everything out.
+        return (1 / d.score) * 100;
+      })
+      .size([svgWidth, svgHeight]);
+
     return {
       svgWidth: svgWidth,
       svgHeight: svgHeight,
       force: force,
       nodes: null,
-      links: null
+      links: null,
     };
   },
+
   componentDidMount: function() {
     var self = this;
+
     // refactor entire graph into sub component - force layout shouldn't be
     // manipulating props, though this works
     this.state.force
               .nodes(this.props.graphData.nodes)
               .links(this.props.graphData.links)
               .start();
-    this.state.force.on("tick", function (tick, b, c) {
+    this.state.force.on('tick', function(tick, b, c) {
       self.forceUpdate();
     });
   },
+
   componentWillReceiveProps: function(nextProps) {
     var self = this;
     this.state.force
               .nodes(nextProps.graphData.nodes)
               .links(nextProps.graphData.links)
               .start();
-    this.state.force.on("tick", function (tick, b, c) {
+    this.state.force.on('tick', function(tick, b, c) {
       self.forceUpdate();
     });
   },
+
   drawLinks: function() {
-    var links = this.props.graphData.links.map(function (link, index) {
-      return (<Link datum={link} key={index} />)
+    var links = this.props.graphData.links.map(function(link, index) {
+      return (<Link datum={link} key={index} />);
     });
+
     return (
-      <g> 
+      <g>
         {links}
       </g>
     );
   },
+
   drawNodes: function() {
-    var nodes = this.props.graphData.nodes.map(function (node, index) {
+    var nodes = this.props.graphData.nodes.map(function(node, index) {
       return (
-        <Node 
+        <Node
           key={index}
           x={node.x}
           y={node.y}
           group={node.group}/>
       );
     });
+
     return nodes;
   },
+
   drawLabels: function() {
     var labels = this.props.graphData.nodes.map(function(node, index) {
       return (
@@ -226,15 +241,17 @@ var Graph = React.createClass({
           text={node.name}/>
       );
     });
+
     return labels;
   },
+
   render: function() {
     return (
       <div>
-        <div style={{"marginLeft": "20px", "fontFamily": "Helvetica"}}>
+        <div style={{marginLeft: '20px', fontFamily: 'Helvetica'}}>
         </div>
         <svg
-          style={{"border": "2px solid black", "margin": "20px"}}
+          style={{border: '2px solid black', margin: '20px'}}
           width={this.state.svgWidth}
           height={this.state.svgHeight}>
           {this.drawLinks()}
@@ -243,7 +260,7 @@ var Graph = React.createClass({
         </svg>
       </div>
     );
-  }
+  },
 });
 
 var App = React.createClass({
@@ -251,14 +268,16 @@ var App = React.createClass({
     return {
       associatedWords: [],
       graphData: {nodes: [], links:[]},
-    }
+    };
   },
+
   componentDidMount: function() {
     this.setState({
       graphData: TEST_DATA,
     });
   },
-  submitWord: function (word) {
+
+  submitWord: function(word) {
     // post word to server
     console.log('post ' + word + ' to server...');
     var self = this;
@@ -267,13 +286,13 @@ var App = React.createClass({
       .post('/word')
       .send({word: word})
       .end(function(err, data) {
-        if (err || !data.ok){
+        if (err || !data.ok) {
           console.log('post request for ' + word + ' failed');
         } else {
           console.log('post request successful: ' + JSON.stringify(data.body));
           var associatedWords = data.body.associations_array;
           var associatedScores = data.body.associations_scored;
-          
+
           // init new graph data object with source word
           var vizData = {nodes:[{name: word, group: 0}], links:[]};
 
@@ -286,12 +305,11 @@ var App = React.createClass({
             var link = {
               source: 0,
               target: i + 1,
-              score: associatedScores[associatedWords[i]] * 300,
+              score: associatedScores[associatedWords[i]],
             };
 
             vizData.nodes.push(node);
             vizData.links.push(link);
-            // console.log('NODE:', node, 'LINK:', link);
           }
 
           self.setState({
@@ -301,12 +319,13 @@ var App = React.createClass({
         }
       });
   },
+
   render: function() {
     return (
       <div>
         <WordInput
           submitWord={this.submitWord}/>
-        <WordList 
+        <WordList
           associatedWords={this.state.associatedWords}/>
         <Graph graphData={this.state.graphData}/>
       </div>
